42c3a4d4084822053ba283aab2ac5bdbece173a4
ludmila davletshina <ludaplv@mail.ru>
2025-02-26 14:58:05 +0300

    добавлено описание

--- network/serializers.py
+++ network/serializers.py
@@ -4,21 +4,21 @@ from network.models import Contact, Product, NetworkLink
 
 
 class ContactSerializer(serializers.ModelSerializer):
-    """Преобразование данных модели "Контакты" из БД в нужный формат"""
+    """Преобразование данных модели "Контакты" из БД в нужный формат."""
     class Meta:
         model = Contact
         fields = "__all__" # Включаем все поля модели
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    """Преобразование данных модели "Продукт" из БД в нужный формат"""
+    """Преобразование данных модели "Продукт" из БД в нужный формат."""
     class Meta:
         model = Product
         fields = "__all__" # Включаем все поля модели
 
 
 class NetworkLinkSerializer(serializers.ModelSerializer):
-    """Преобразование данных модели "Торговая сеть" из БД в нужный формат"""
+    """Преобразование данных модели "Торговая сеть" из БД в нужный формат."""
 
     # указываем серилизаторы отдельных объектов, чтобы данные этих объектов отображалались корректно в API
     contact = ContactSerializer() # используем "OneToOneField" - это отдельный объект
@@ -31,7 +31,7 @@ class NetworkLinkSerializer(serializers.ModelSerializer):
 
 
 class NetworkLinkCreateSerializer(serializers.ModelSerializer):
-    """Создание и обновление объектов модели "Торговая сеть"""
+    """Создание и обновление объектов модели "Торговая сеть."""
     class Meta:
         model = NetworkLink
         fields = '__all__' # Включаем все поля модели
